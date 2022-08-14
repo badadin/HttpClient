15460a91d88139dfa2ee222b13342aed56e9676f
badadin <badadinf@mail.ru>
2022-08-14 13:04:29 +0400

    lib test usage

--- sample/src/com/rinatvasilev/httpclient/App.kt
+++ sample/src/com/rinatvasilev/httpclient/App.kt
@@ -7,6 +7,18 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val httpClient = HttpClient.Builder()
+            .baseUrl("test base url")
+            .build()
+
+        val httpClientInterfaceImpl: SomeService = httpClient.create()
+        httpClientInterfaceImpl.getList(id = 123)
+
         //todo init keystore
     }
+}
+
+interface SomeService {
+    @Post("some url here")
+    fun getList(@Param("id") id: Long)
 }
