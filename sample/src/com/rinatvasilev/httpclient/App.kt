7dd10e3361ecead4c63317f93067789b9f67e7e5
badadin <badadinf@mail.ru>
2022-08-27 19:07:36 +0400

    cleanup code

--- sample/src/com/rinatvasilev/httpclient/App.kt
+++ sample/src/com/rinatvasilev/httpclient/App.kt
@@ -20,51 +20,12 @@ class App : Application() {
         catService = httpClient.create()
 
         //todo init keystore
-
-        // https://cataas.com/cat?id=595f280c557291a9750ebf80
-        // конкретная картинка с кошаком
-
-        // https://cataas.com/api/cats?tags=cute&limit=10
-        /*
-        [
-           {
-              "id":"595f280c557291a9750ebf80",
-              "created_at":"2015-11-06T18:36:37.342Z",
-              "tags":[
-                 "cute",
-                 "eyes"
-              ]
-           },
-           {
-              "id":"595f280e557291a9750ebf9f",
-              "created_at":"2016-10-09T12:51:24.421Z",
-              "tags":[
-                 "cute",
-                 "sleeping"
-              ]
-           },
-           {
-              "id":"595f280e557291a9750ebfa6",
-              "created_at":"2016-11-22T15:20:31.913Z",
-              "tags":[
-                 "cute",
-                 "sleeping"
-              ]
-           }
-        ]
-        */
     }
 }
 
 interface CatService {
-    @Post("cat/cute")
-    fun getRandomCuteCatWithPost(@Param("id") id: Long): HttpClientResponse
-
-    @Get("cat/cute")
-    fun getRandomCuteCatWithGet(@Param("id") id: Long, @Param("name") name: String): HttpClientResponse
-
-    @Get("cat/cute")
-    fun getRandomCuteCatWithGetNoParams(): HttpClientResponse
+    @Get("cat")
+    fun getCatById(@Param("id") id: Long): HttpClientResponse
 
     @Get("api/cats")
     fun getCuteCats(@Param("tags") tags: String, @Param("limit") limit: Int): HttpClientResponse
